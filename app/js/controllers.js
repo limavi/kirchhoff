535e1ab9f15e4362c14f8a6fed25c6ce09cf3391
Lili <lilianamanjarres@hotmail.com>
2017-11-01 05:49:54 -0500

    modificacion

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,39 +1,57 @@
   angular.module('ModuleApp', [])
     .controller('TodosLosLibrosCtrl', ['$scope', function($scope) {
 
+    // CIRCUITO #1
 	 $scope.CalcularCircuito1 = function(circuito1) {
-	 	$scope.circuito1.lvk1= circuito1.V1 + "  +  " + circuito1.R1 +"I1  -  " +  circuito1.V2 + "    -  " + circuito1.R2 + "I2"
-	 	$scope.circuito1.lvk2= circuito1.V1 + "  +  " + circuito1.R1 +"I1  +  " +  circuito1.R3 + "I3  -  " + circuito1.V3 
-
-        var F1={
-     		I1:1,
-     		I2:1,
-     		I3:-1,
-     		Result:0
-     	}
-
+	 	$scope.circuito1.lvk1= circuito1.V1 + "  +  " + circuito1.R1 +"I1  -  " +  circuito1.V2 + "    -  " + circuito1.R2 + "I2 = 0"
+	 	$scope.circuito1.lvk2= circuito1.V1 + "  +  " + circuito1.R1 +"I1  +  " +  circuito1.R3 + "I3  -  " + circuito1.V3  + "= 0"
+        var F1={I1:1, I2:1, I3:-1, Result:0}
      	var F2={
      		I1:circuito1.R1,
      		I2:-circuito1.R2,
      		I3:0,
      		Result: -(circuito1.V1 - circuito1.V2)
      	}
-
      	var F3={
      		I1:circuito1.R1,
      		I2: 0,
      		I3:circuito1.R3,
      		Result:-(circuito1.V1 - circuito1.V3)
      	}
-
+        //FORMULA PARA RESOLVER 3 ECUACIONES CON 3 INCOGNITAS
      	var defer=(F1.I1*F2.I2*F3.I3) + (F2.I1*F3.I2*F1.I3) + (F3.I1*F1.I2*F2.I3) - (F1.I3*F2.I2*F3.I1) - (F2.I3*F3.I2*F1.I1) - (F3.I3*F1.I2*F2.I1);
  		$scope.circuito1.i1=(F1.Result*F2.I2*F3.I3  +  F2.Result*F3.I2*F1.I3  +  F3.Result*F1.I2*F2.I3  -  F1.I3*F2.I2*F3.Result  - F2.I3*F3.I2*F1.Result  -F3.I3*F1.I2*F2.Result)/defer;
  		$scope.circuito1.i2=(F1.I1*F2.Result*F3.I3  + F2.I1*F3.Result*F1.I3  +  F3.I1*F1.Result*F2.I3  - F1.I3*F2.Result*F3.I1  - F2.I3*F3.Result*F1.I1  -F3.I3*F1.Result*F2.I1)/defer;
  		$scope.circuito1.i3=(F1.I1*F2.I2*F3.Result  +  F2.I1+F3.I2*F1.Result  + F3.I1*F1.I2*F2.Result  - F1.Result*F2.I2*F3.I1  -F2.Result*F3.I2*F1.I1  - F3.Result*F1.I2*F2.I1)/defer;
       };
 
-	 $scope.limpiarFormCircuito1 = function() {
-	 	$scope.circuito1={};
+      $scope.limpiarFormCircuito1 = function() {
+        $scope.circuito1={};
+      };
+
+
+    // CIRCUITO #2
+    $scope.CalcularCircuito2 = function(circuito2) {
+        $scope.circuito2.lvk1= "-" + circuito2.V1 + "  +  " + circuito2.R1 +"I1  -  " +  circuito2.R3 + "(I1 - I2)   = 0 " 
+        $scope.circuito2.lvk2= circuito2.R3 + "(I2 - I1) +  " + circuito2.R2 +"I2  -  " + circuito2.V2  + " = 0 "
+        var F1={I1:-1, I2:1, I3:1, Result:0}
+        var F2={
+            I1:circuito2.R1 + circuito2.R3 ,
+            I2:-circuito2.R3,
+            I3:0,
+            Result: circuito2.V1
+        }
+        var F3={
+            I1:-circuito2.R3,
+            I2: circuito2.R2 + circuito2.R3,
+            I3:0,
+            Result: circuito2.V2
+        }
+        //FORMULA PARA RESOLVER 3 ECUACIONES CON 3 INCOGNITAS
+        var defer=(F1.I1*F2.I2*F3.I3) + (F2.I1*F3.I2*F1.I3) + (F3.I1*F1.I2*F2.I3) - (F1.I3*F2.I2*F3.I1) - (F2.I3*F3.I2*F1.I1) - (F3.I3*F1.I2*F2.I1);
+        $scope.circuito2.i1=(F1.Result*F2.I2*F3.I3  +  F2.Result*F3.I2*F1.I3  +  F3.Result*F1.I2*F2.I3  -  F1.I3*F2.I2*F3.Result  - F2.I3*F3.I2*F1.Result  -F3.I3*F1.I2*F2.Result)/defer;
+        $scope.circuito2.i2=(F1.I1*F2.Result*F3.I3  + F2.I1*F3.Result*F1.I3  +  F3.I1*F1.Result*F2.I3  - F1.I3*F2.Result*F3.I1  - F2.I3*F3.Result*F1.I1  -F3.I3*F1.Result*F2.I1)/defer;
+        $scope.circuito2.i3=(F1.I1*F2.I2*F3.Result  +  F2.I1+F3.I2*F1.Result  + F3.I1*F1.I2*F2.Result  - F1.Result*F2.I2*F3.I1  -F2.Result*F3.I2*F1.I1  - F3.Result*F1.I2*F2.I1)/defer;
       };
 
      $scope.limpiarFormCircuito2 = function() {
