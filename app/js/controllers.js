b69e0429ab5113ee26e082a6aa318b3ef8a5a24c
Lili <lilianamanjarres@hotmail.com>
2017-11-01 05:03:21 -0500

    modification

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,9 +2,8 @@
     .controller('TodosLosLibrosCtrl', ['$scope', function($scope) {
 
 	 $scope.CalcularCircuito1 = function(circuito1) {
-	 	$scope.lvk1= circuito1.V1 + "  +  " + circuito1.R1 +"I1  -  " +  circuito1.V2  + "  -  " + circuito1.R2 + "I2"
-	 	$scope.lvk2= circuito1.V1 + "  +  " + circuito1.R1 +"I1  +  " +  circuito1.R3 + "I3  - " + circuito1.V3 
-     
+	 	$scope.circuito1.lvk1= circuito1.V1 + "  +  " + circuito1.R1 +"I1  -  " +  circuito1.V2 + "    -  " + circuito1.R2 + "I2"
+	 	$scope.circuito1.lvk2= circuito1.V1 + "  +  " + circuito1.R1 +"I1  +  " +  circuito1.R3 + "I3  -  " + circuito1.V3 
 
         var F1={
      		I1:1,
@@ -27,20 +26,18 @@
      		Result:-(circuito1.V1 - circuito1.V3)
      	}
 
-     	alert(F1,F2,F3)
-
-     	var defer=(F1.I1*F2.I2*F3.I3) + (F2.I1*F3.I2*F1.I3) + (F3.I1*F1.I2*F2.I3) - (F1.I3*F2.I2*F3.I1) - (F2.I3*F3.I2*F1.I1) - (F3.I3*F1.I2*F2.I1) 
-
- 		var i1=(F1.Result*F2.I2*F3.I3  +  F2.Result*F3.I2*F1.I3  +  F3.Result*F1.I2*F2.I3  -  F1.I3*F2.I2*F3.Result  - F2.I3*F3.I2*F1.Result  -F3.I3*F1.I2*F2.Result)/defer;
-
- 		$scope.result = "I1 = " + i1
-
+     	var defer=(F1.I1*F2.I2*F3.I3) + (F2.I1*F3.I2*F1.I3) + (F3.I1*F1.I2*F2.I3) - (F1.I3*F2.I2*F3.I1) - (F2.I3*F3.I2*F1.I1) - (F3.I3*F1.I2*F2.I1);
+ 		$scope.circuito1.i1=(F1.Result*F2.I2*F3.I3  +  F2.Result*F3.I2*F1.I3  +  F3.Result*F1.I2*F2.I3  -  F1.I3*F2.I2*F3.Result  - F2.I3*F3.I2*F1.Result  -F3.I3*F1.I2*F2.Result)/defer;
+ 		$scope.circuito1.i2=(F1.I1*F2.Result*F3.I3  + F2.I1*F3.Result*F1.I3  +  F3.I1*F1.Result*F2.I3  - F1.I3*F2.Result*F3.I1  - F2.I3*F3.Result*F1.I1  -F3.I3*F1.Result*F2.I1)/defer;
+ 		$scope.circuito1.i3=(F1.I1*F2.I2*F3.Result  +  F2.I1+F3.I2*F1.Result  + F3.I1*F1.I2*F2.Result  - F1.Result*F2.I2*F3.I1  -F2.Result*F3.I2*F1.I1  - F3.Result*F1.I2*F2.I1)/defer;
       };
 
 	 $scope.limpiarFormCircuito1 = function() {
 	 	$scope.circuito1={};
-	 	$scope.lvk1= ""
-	 	$scope.lvk2= ""
+      };
+
+     $scope.limpiarFormCircuito2 = function() {
+        $scope.circuito2={};
       };
 
    }]);
